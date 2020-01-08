5d0c5bae91af5186e692aa548553abe59704444e
AJ Hoffer <aj@hoffer.fm>
2020-01-08 15:00:52 -0500

    update data export

--- Blog/Controllers/AdminController.cs
+++ Blog/Controllers/AdminController.cs
@@ -4,6 +4,9 @@ using BC.Data.Context;
 using BC.Models;
 using Newtonsoft.Json;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -23,26 +26,41 @@ namespace BC.Controllers
         public ActionResult ExportDataToJson()
         {
             var db = new DBDataContext();
+            var _categories = new Repository<BC_Category>(db).All().ToList();
+            var _tags = new Repository<BC_Tag>(db).All().ToList();
+            var _comments = new Repository<BC_Comment>(db).All().ToList();
             var data = new Hashtable();
 
             var _pages = new Repository<BC_Page>(db);
             data.Add("Pages", _pages.All().ToList());
 
-            var _categories = new Repository<BC_Category>(db);
-            data.Add("Categories", _categories.All().ToList());
-
-            var _tags = new Repository<BC_Tag>(db);
-            data.Add("Tags", _tags.All().ToList());
-
             var _posts = new Repository<BC_Post>(db);
-            data.Add("Posts", _posts.All().ToList());
+            var posts = new List<dynamic>();
+            foreach(var post in _posts.All().ToList())
+            {
+                var dynamicPost = ToDynamic(post);
+                dynamicPost.Category = _categories.FirstOrDefault(x => x.CategoryID == post.CategoryID);
+                dynamicPost.Tags = _tags.Where(x => x.PostID == post.PostID).OrderBy(x => x.Name).Select(x => x.Name).ToList();
+                dynamicPost.Comments = _comments.Where(x => x.PostID == post.PostID).OrderBy(x => x.Created).ToList();
 
-            var _comments = new Repository<BC_Comment>(db);
-            data.Add("Comments", _comments.All().ToList());
+                posts.Add(dynamicPost);
+            }
+
+            data.Add("Posts", posts);
 
             var json = JsonConvert.SerializeObject(data, Formatting.Indented);
 
             return Content(json, "json");
+        }
+
+        private dynamic ToDynamic(object value)
+        {
+            IDictionary<string, object> expando = new ExpandoObject();
+
+            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(value.GetType()))
+                expando.Add(property.Name, property.GetValue(value));
+
+            return expando as ExpandoObject;
         }
     }
 }
