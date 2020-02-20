9818923a4e32371a2496dd03af15afb4493041d8
AJ Hoffer <aj@hoffer.fm>
2020-02-20 15:00:53 -0500

    remove tiny_mce

--- Blog/Scripts/Pages/pageAdmin.edit.js
+++ Blog/Scripts/Pages/pageAdmin.edit.js
@@ -1,16 +1,4 @@
-﻿tinyMCE.init({
-    // General options
-    mode: "textareas",
-    theme: "advanced",
-    theme_advanced_buttons1: "code,cleanup,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent,indent,blockquote,|,bullist,numlist,|,link,unlink,|,styleselect,formatselect,fontselect,fontsizeselect",
-    theme_advanced_buttons2: "",
-    theme_advanced_buttons3: "",
-    theme_advanced_toolbar_location: "top",
-    theme_advanced_toolbar_align: "left",
-    theme_advanced_statusbar_location: "bottom"
-});
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $("input[type=checkbox]").checkbox();
 
     $("#Title").blur(function () {
