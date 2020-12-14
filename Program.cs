ceedf6c764403f50bc4f7db03a2394f9ecff0c5c
AJ Hoffer <aj@hoffer.fm>
2020-12-14 14:50:29 -0500

    update url structure

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace dochoffiday
                         if (ctx.PipelineName == nameof(Statiq.Web.Pipelines.Content))
                         {
                             return doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
-                                ? new NormalizedPath(doc.GetDateTime(WebKeys.Published).ToString("yyyy")).Combine(doc.GetString("Category")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
+                                ? new NormalizedPath(doc.GetString("Category")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
                                 : doc.Destination.ChangeExtension(".html");
                         }
 
