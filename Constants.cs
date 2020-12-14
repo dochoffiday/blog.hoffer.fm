8f57ab45665e0d4120bfc44522504c3315f4d3dc
AJ Hoffer <aj@hoffer.fm>
2020-12-14 14:09:24 -0500

    update site uri

--- Constants.cs
+++ Constants.cs
@@ -7,7 +7,7 @@ namespace dochoffiday
     public class Constants
     {
         public const string SiteTitle = "AJ Hoffer";
-        public const string SiteUri = "https://example.com";
+        public const string SiteUri = "https://blog.hoffer.fm";
         public const string BlogTitle = "AJ Hoffer's Blog";
         public const int PostsPerPage = 10;
     }
